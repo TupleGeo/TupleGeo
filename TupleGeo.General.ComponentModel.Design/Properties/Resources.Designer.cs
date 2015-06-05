9a8f5f52ef6d0aaa55fb46cac6bae7f6f895e675
gisvlasta <vasilis.vlastaras@gmail.com>
2015-06-05 04:47:45 +0100

    Base Version 2.0.0 - .NET 40

--- TupleGeo.General.ComponentModel.Design/Properties/Resources.Designer.cs
+++ TupleGeo.General.ComponentModel.Design/Properties/Resources.Designer.cs
@@ -39,7 +39,7 @@ namespace TupleGeo.General.ComponentModel.Design.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("TupleGeo.Global.ComponentModel.Design.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("TupleGeo.General.ComponentModel.Design.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;

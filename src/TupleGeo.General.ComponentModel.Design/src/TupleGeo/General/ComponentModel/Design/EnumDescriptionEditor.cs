1414c2f31a7cecf26b524fd6fe12e21c53744297
gisvlasta <vasilis.vlastaras@gmail.com>
2015-06-08 03:44:33 +0100

    a bit more

---
+++ src/TupleGeo.General.ComponentModel.Design/src/TupleGeo/General/ComponentModel/Design/EnumDescriptionEditor.cs
@@ -0,0 +1,232 @@
+﻿
+#region Header
+// Title Name       : EnumDescriptionEditor
+// Member of        : TupleGeo.General.ComponentModel.Design.dll
+// Description      : Provides an editor for enumerations that is displaying value descriptions.
+// Created by       : 03/03/2010, 17:23, Vasilis Vlastaras.
+// Updated by       : 23/02/2010, 16:57, Vasilis Vlastaras.
+//                    1.0.1 - Added preprocessor directives to make the source file compatible with .NET Framework 2.0.
+//                  : 23/05/2015, 07:44, Vasilis Vlastaras.
+//                    1.0.2 - Changed the code to make the editor behaving better.
+// Version          : 1.0.2
+// Contact Details  : TupleGeo.
+// License          : Apache License.
+// Copyright        : TupleGeo, 2010 - 2015.
+// Comments         : 
+#endregion
+
+#region Imported Namespaces
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing.Design;
+
+#if !NET20
+using System.Linq;
+#endif
+
+using System.Text;
+using System.Windows.Forms;
+using System.Windows.Forms.Design;
+
+#endregion
+
+namespace TupleGeo.General.ComponentModel.Design {
+
+  /// <summary>
+  /// Provides an editor for enumerations that is displaying value descriptions.
+  /// </summary>
+  public sealed class EnumDescriptionEditor : UITypeEditor, IDisposable {
+
+    #region Member Variables
+
+    private IWindowsFormsEditorService _windowsFormsEditorService;
+    private EnumDescriptionEditorControl _enumDescriptionControl = null;
+    private string _lastCultureUsed;
+
+    #endregion
+
+    #region Constructors - Destructors
+
+    /// <summary>
+    /// Initializes the <see cref="EnumDescriptionEditor"/>.
+    /// </summary>
+    public EnumDescriptionEditor() {
+      if (_enumDescriptionControl == null) {
+        _enumDescriptionControl = new EnumDescriptionEditorControl();
+      }
+    }
+
+    #endregion
+
+    #region Event Procedures
+
+    /// <summary>
+    /// Occurs when the user clicks the enumeration description control.
+    /// </summary>
+    /// <param name="sender">The sender of the event.</param>
+    /// <param name="e">The <see cref="MouseEventArgs"/>.</param>
+    private void _enumDescriptionControl_MouseClick(object sender, MouseEventArgs e) {
+      _windowsFormsEditorService.CloseDropDown();
+    }
+
+    /// <summary>
+    /// Occurs when the user presses a key on the enumeration description control.
+    /// </summary>
+    /// <param name="sender">The sender of the event.</param>
+    /// <param name="e">The <see cref="KeyEventArgs"/>.</param>
+    private void _enumDescriptionControl_KeyDown(object sender, KeyEventArgs e) {
+      _windowsFormsEditorService.CloseDropDown();
+    }
+
+    #endregion
+
+    #region UITypeEditor
+
+    /// <summary>
+    /// Edits a <see cref="string"/> value.
+    /// </summary>
+    /// <param name="context">The <see cref="ITypeDescriptorContext"/>.</param>
+    /// <param name="provider">The <see cref="IServiceProvider"/>.</param>
+    /// <param name="value">The value to be edited.</param>
+    /// <returns>An <see cref="object"/> storing the edited <see cref="string"/>.</returns>
+    public override object EditValue(
+      ITypeDescriptorContext context,
+      IServiceProvider provider,
+      object value
+    ) {
+
+      if (value == null) {
+        throw new ArgumentNullException("value");
+      }
+      
+      if (!value.GetType().IsEnum) {
+        throw new ArgumentException("Invalid value type.", "value");
+      }
+
+      if (provider == null) {
+        throw new ArgumentNullException("provider");
+      }
+      
+      // Set the windows service.
+      this._windowsFormsEditorService =
+        (IWindowsFormsEditorService)provider.GetService(typeof(IWindowsFormsEditorService));
+
+      // Unsubscribe / Subscribe to enumeration description control events.
+      _enumDescriptionControl.MouseClick -= new System.Windows.Forms.MouseEventHandler(_enumDescriptionControl_MouseClick);
+      _enumDescriptionControl.MouseClick += new System.Windows.Forms.MouseEventHandler(_enumDescriptionControl_MouseClick);
+
+      _enumDescriptionControl.KeyDown -= new System.Windows.Forms.KeyEventHandler(_enumDescriptionControl_KeyDown);
+      _enumDescriptionControl.KeyDown += new System.Windows.Forms.KeyEventHandler(_enumDescriptionControl_KeyDown);
+
+      if (_enumDescriptionControl.EnumDescriptionsCollection != null) {
+        // Load descriptions in the control.
+        if (Application.CurrentCulture.ToString() != _lastCultureUsed) {
+          _enumDescriptionControl.EnumDescriptionsCollection.Clear();
+
+          _lastCultureUsed = Application.CurrentCulture.ToString();
+
+          // Get the enumeration value.
+          Enum enumValue = (Enum)value;
+
+          // Get the enumeration names.
+          string[] names = Enum.GetNames(value.GetType());
+
+          // Try to get the enumeration descriptions for the current application culture.
+          string[] descriptions = EnumDescriptionConverter.GetEnumDescriptions(enumValue, _lastCultureUsed);
+
+          // Test if names and descriptions are the same.
+          // If this is the case then descriptions in current culture where not found.
+          bool areTheSame = true;
+          for (int i = 0; i < names.Length; i++) {
+            if (names[i] != descriptions[i]) {
+              areTheSame = false;
+              break;
+            }
+          }
+          
+          if (areTheSame) {
+            // Since no descriptions found for the current culture, try to get the neutral culture descriptions.
+            descriptions = EnumDescriptionConverter.GetEnumDescriptions(enumValue);
+          }
+          
+          // Ending up here either the current culture descriptions,
+          // neutral culture descriptions or enumeration named value have been retrieved.
+          for (int i = 0; i < descriptions.Length; i++) {
+            _enumDescriptionControl.EnumDescriptionsCollection.Add(new EnumNameDescriptionPair(names[i], descriptions[i]));
+          }
+
+          // Sort the collection and bind the data to the control.
+          _enumDescriptionControl.SortEnumDescriptionsCollection();
+          _enumDescriptionControl.DataBind();
+          
+          // Set the selected item on the control.
+          _enumDescriptionControl.SetSelectedEnumValueName(enumValue.ToString());
+          
+        }
+
+        // Show the enumeration editing control.
+        _enumDescriptionControl.Show();
+        this._windowsFormsEditorService.DropDownControl(_enumDescriptionControl);
+
+        if (_enumDescriptionControl.SelectedEnumValueName != null) {
+          // Return the enumerated value once the user has selected one.
+          return Enum.Parse(value.GetType(), (string)_enumDescriptionControl.SelectedEnumValueName);
+        }
+        else {
+          return null;
+        }
+      }
+      else {
+        return null;
+      }
+      
+    }
+
+    /// <summary>
+    /// Gets the <see cref="UITypeEditorEditStyle">edit style</see> of the editor window.
+    /// </summary>
+    /// <param name="context">The <see cref="ITypeDescriptorContext"/>.</param>
+    /// <returns>The <see cref="UITypeEditorEditStyle"/>.</returns>
+    public override UITypeEditorEditStyle GetEditStyle(ITypeDescriptorContext context) {
+      return UITypeEditorEditStyle.DropDown;
+    }
+
+    /// <summary>
+    /// Gets whether the paint value is supported or not.
+    /// </summary>
+    /// <param name="context">The <see cref="ITypeDescriptorContext"/>.</param>
+    /// <returns>
+    /// A <see cref="bool"/> indicating whether the paint value is supported or not.
+    /// </returns>
+    public override bool GetPaintValueSupported(ITypeDescriptorContext context) {
+      return false;
+    }
+
+    /// <summary>
+    /// Indicates whether the dropdown window is resizable or not.
+    /// </summary>
+    public override bool IsDropDownResizable {
+      get {
+        return true;
+      }
+    }
+
+    #endregion
+
+    #region IDisposable Members
+
+    /// <summary>
+    /// Disposes the <see cref="EnumDescriptionEditor"/> which subsequently
+    /// disposes its associated <see cref="EnumDescriptionEditorControl"/>. 
+    /// </summary>
+    public void Dispose() {
+      _enumDescriptionControl.Dispose();
+    }
+
+    #endregion
+
+  }
+
+}
---
+++ src/TupleGeo.General.ComponentModel.Design/src/TupleGeo/General/ComponentModel/Design/GeneralObjectEditorControl.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace TupleGeo.General.ComponentModel.Design {
+  partial class GeneralObjectEditorControl {
+    /// <summary> 
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary> 
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing) {
+      if (disposing && (components != null)) {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Component Designer generated code
+
+    /// <summary> 
+    /// Required method for Designer support - do not modify 
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent() {
+      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GeneralObjectEditorControl));
+      this.GeneralObjectEditorToolStrip = new System.Windows.Forms.ToolStrip();
+      this.NewObjectToolStripButton = new System.Windows.Forms.ToolStripButton();
+      this.DeleteObjectToolStripButton = new System.Windows.Forms.ToolStripButton();
+      this.GeneralObjectEditorToolStrip.SuspendLayout();
+      this.SuspendLayout();
+      // 
+      // GeneralObjectEditorToolStrip
+      // 
+      this.GeneralObjectEditorToolStrip.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+      this.GeneralObjectEditorToolStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
+      this.GeneralObjectEditorToolStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.NewObjectToolStripButton,
+            this.DeleteObjectToolStripButton});
+      resources.ApplyResources(this.GeneralObjectEditorToolStrip, "GeneralObjectEditorToolStrip");
+      this.GeneralObjectEditorToolStrip.Name = "GeneralObjectEditorToolStrip";
+      // 
+      // NewObjectToolStripButton
+      // 
+      resources.ApplyResources(this.NewObjectToolStripButton, "NewObjectToolStripButton");
+      this.NewObjectToolStripButton.Name = "NewObjectToolStripButton";
+      this.NewObjectToolStripButton.Click += new System.EventHandler(this.NewObjectToolStripButton_Click);
+      // 
+      // DeleteObjectToolStripButton
+      // 
+      resources.ApplyResources(this.DeleteObjectToolStripButton, "DeleteObjectToolStripButton");
+      this.DeleteObjectToolStripButton.Name = "DeleteObjectToolStripButton";
+      this.DeleteObjectToolStripButton.Click += new System.EventHandler(this.DeleteObjectToolStripButton_Click);
+      // 
+      // GeneralObjectEditorControl
+      // 
+      resources.ApplyResources(this, "$this");
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.Controls.Add(this.GeneralObjectEditorToolStrip);
+      this.Name = "GeneralObjectEditorControl";
+      this.GeneralObjectEditorToolStrip.ResumeLayout(false);
+      this.GeneralObjectEditorToolStrip.PerformLayout();
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.ToolStrip GeneralObjectEditorToolStrip;
+    private System.Windows.Forms.ToolStripButton NewObjectToolStripButton;
+    private System.Windows.Forms.ToolStripButton DeleteObjectToolStripButton;
+  }
+}

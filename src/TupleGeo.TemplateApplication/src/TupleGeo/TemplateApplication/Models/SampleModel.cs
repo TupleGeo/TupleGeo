b3843d1c659cda33bea6e9bc987b9a7218d3134d
Vasilis <vasilis.vlastaras@gmail.com>
2015-06-08 21:35:39 +0100

    TupleGeo.TemplateApplications

---
+++ src/TupleGeo.TemplateApplication/src/TupleGeo/TemplateApplication/Models/SampleModel.cs
@@ -0,0 +1,69 @@
+﻿
+#region Header
+
+#endregion
+
+#region Imported Namespaces
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TupleGeo.General.ComponentModel;
+using TupleGeo.Apps;
+
+#endregion
+
+namespace TupleGeo.TemplateApplication.Models {
+
+  /// <summary>
+  /// 
+  /// </summary>
+  public sealed class SampleModel : ObservableObject<SampleModel>, IModel {
+
+    #region Member Variables
+
+    #endregion
+
+    #region Constructors - Destructors
+
+    /// <summary>
+    /// Initializes the <see cref="SampleModel"/>.
+    /// </summary>
+    public SampleModel() {
+      
+    }
+
+    #endregion
+
+    #region Public Properties
+
+    #endregion
+
+    #region Event Procedures
+
+    #endregion
+
+    #region Private Procedures
+
+    #endregion
+
+    #region IModel Members
+
+    private const string _modelName = "SampleModel";
+
+    /// <summary>
+    /// Gets the name of the model.
+    /// </summary>
+    public string ModelName {
+      get {
+        return _modelName;
+      }
+    }
+
+    #endregion
+
+  }
+
+}

1d884afd2c211a935501cfb12b7abd58f73071a8
Vasilis <vasilis.vlastaras@gmail.com>
2015-06-08 17:09:57 +0100

    TupleGeo.Resources

---
+++ src/TupleGeo.Resources/Properties/AssemblyInfo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TupleGeo.Resources")]
+[assembly: AssemblyDescription("Provides a set of resources.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("TupleGeo")]
+[assembly: AssemblyProduct("TupleGeo.Resources")]
+[assembly: AssemblyCopyright("Copyright © 2009 - 2015, TupleGeo.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("97fc7bca-d465-4de4-9571-2a75c0cbad9f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.1.4.*")]
+[assembly: AssemblyFileVersion("1.1.4")]
+
+[assembly: CLSCompliant(true)]
+[assembly: NeutralResourcesLanguage("en-US")]
